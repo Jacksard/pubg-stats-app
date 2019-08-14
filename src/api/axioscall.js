00eefdca21653059e33fa06691de77d1c7c9b81d
Jacksard <jacobgoskha@gmail.com>
2019-08-14 09:24:28 -0400

    error handle : player not found

--- src/api/axioscall.js
+++ src/api/axioscall.js
@@ -12,29 +12,5 @@ export const callPlayer = playerId => {
     });
 };
 
-/* export const callLifetime = accountId => {
-  console.log(accountId);
-  return axios
-    .get(url.lifetime + accountId, {})
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}; */
 
-/* export const callMatch = (type, matchId) => {
-  return axios
-    .get(type + matchId, {})
-    .then(response => {
-      return response.data;
-      console.log(user.data[0]);
-    
-      var join = user.data[0]; 
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}; */
 export const callSeason = (type, playerId) => { };
