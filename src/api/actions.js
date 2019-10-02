7fd09d4d9bd4ac4f96f567109f39db54b404493d
Jacob Goskha <jacobgoskha@gmail.com>
2019-10-02 13:16:18 -0400

    point to server

--- src/api/actions.js
+++ src/api/actions.js
@@ -1,12 +1,12 @@
 export const urlSeasons = 'https://api.pubg.com/shards/steam/seasons';
 
 export const url = {
-  player: 'http://localhost:5000/api/pubg/player/',
+  /* player: 'http://localhost:5000/api/pubg/player/',
   match: 'https://api.pubg.com/shards/steam/matches/',
-  lifetime: 'http://localhost:5000/api/pubg/lifetime/'
-  /* player: 'https://shielded-beyond-95672.herokuapp.com/api/pubg/player/',
+  lifetime: 'http://localhost:5000/api/pubg/lifetime/' */
+  player: 'https://shielded-beyond-95672.herokuapp.com/api/pubg/player/',
   match: 'https://api.pubg.com/shards/steam/matches/',
-  lifetime: 'https://shielded-beyond-95672.herokuapp.com/api/pubg/lifetime/' */
+  lifetime: 'https://shielded-beyond-95672.herokuapp.com/api/pubg/lifetime/'
 };
 
 export const examplePlayerId = 'account.ae70c0ffc0db479ab5b464c25f702f48';
