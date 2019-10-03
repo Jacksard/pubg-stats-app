aca6e21bc560442ba6418f11bff6b83492de359b
Jacob Goskha <jacobgoskha@gmail.com>
2019-10-03 09:55:47 -0400

    stats arrays for comparison mapped

--- src/Components/CurrentGameType/CurrentGameType.js
+++ src/Components/CurrentGameType/CurrentGameType.js
@@ -8,7 +8,7 @@ class currentGameType extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comparison: {
+      comparisonData: {
         kd: null,
         kills: null,
         top10s: null,
@@ -19,26 +19,59 @@ class currentGameType extends Component {
   }
 
   componentDidMount() {
+    const indexHighValue = array => {
+      const indexOfMaxValue = array.reduce(
+        (indexMax, x, i, arr) => (x > arr[indexMax] ? i : indexMax),
+        0
+      );
+      return indexOfMaxValue;
+    };
     // Map all comparison variables here
+    // K/D
     const kd = this.props.data.map(item =>
-      (
+      parseFloat(
         item.currentSeason.data.attributes.gameModeStats['solo-fpp'].kills /
-        item.currentSeason.data.attributes.gameModeStats['solo-fpp'].losses
+          item.currentSeason.data.attributes.gameModeStats['solo-fpp'].losses
       ).toFixed(2)
     );
-
+    console.log(kd);
+    // Wins
+    const wins = this.props.data.map(
+      item => item.currentSeason.data.attributes.gameModeStats['solo-fpp'].wins
+    );
+    // Kills
     const kills = this.props.data.map(
       item => item.currentSeason.data.attributes.gameModeStats['solo-fpp'].kills
     );
+    // Top 10's
+    const top10s = this.props.data.map(
+      item =>
+        item.currentSeason.data.attributes.gameModeStats['solo-fpp'].top10s
+    );
+    // Longest Kill
+    const longestKill = this.props.data.map(item =>
+      item.currentSeason.data.attributes.gameModeStats[
+        'solo-fpp'
+      ].longestKill.toFixed(0)
+    );
+    // Kills
+    const headshotKills = this.props.data.map(
+      item =>
+        item.currentSeason.data.attributes.gameModeStats['solo-fpp']
+          .headshotKills
+    );
 
     console.log(kd);
+    console.log(wins);
     console.log(kills);
+    console.log(top10s);
+    console.log(longestKill);
+    console.log(headshotKills);
 
-    console.log(kills);
-    const highestKill = Math.max.apply(Math, kills);
-
-    this.setState({ comparison: [null] });
-    console.log(this.state.comparison);
+    const comparisonCopy = this.state.comparisonData;
+    comparisonCopy.kd = indexHighValue(kd);
+    this.setState({ comparisonData: comparisonCopy });
+    console.log(this.state.comparisonData);
   }
 
   handleGameType(type) {
@@ -49,7 +82,7 @@ class currentGameType extends Component {
             data={this.props.data}
             index={this.props.index}
             view={this.props.view}
-            comparison={this.state.comparison}
+            comparison={this.state.comparisonData}
           />
         );
       case 'duo':
